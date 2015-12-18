4e0203905b11e5f1c017a38dee501fea98c1f913
Jacqueline Homan <jacquelinehoman7@gmail.com>
2015-12-18 06:26:03 -0500

    first commit

---
+++ Domain.fs
@@ -0,0 +1,38 @@
+﻿namespace MovieNews.Data
+open System
+
+type Review =
+    { Published : DateTime
+      Summary : string
+      Link : string
+      LinkText : string }
+
+type Cast = 
+    { Actor : string
+      Character : string }
+
+type MovieBasics =
+    { Title : string
+      Summary : string
+      Thumbnail : option<string> }
+     
+type MovieDetails =
+    { Homepage : string
+      Genres : seq<string>
+      Overview : string
+      Companies : seq<string>
+      Poster : string
+      Countries : seq<string>
+      Released : DateTime
+      AverageVote : decimal }
+
+type Movie =
+    { Movie : MovieBasics
+      Details : MovieDetails
+      Review : option<Review>
+      Cast : seq<Cast> }
+
+
+
+
+
---
+++ Program.fs
@@ -0,0 +1,28 @@
+﻿open System
+open System.IO
+open MovieNews.Data
+
+let findByTitle title =
+    if title = "Interstellar" then
+        { Published = DateTime(2014, 11, 5)
+          Summary = 
+              "In Christopher Nolan's science-fiction parablee " +
+              "Interstellar, Earth is dying, and a team of " +
+              "astronauts searches the universe for a new home " +
+              "for the human race."
+          Link = "http://movies.nytimes.com/2014/11/05/movies/interstellar"
+          LinkText = "Read the NY Times review" } |> Some
+    else None
+
+let reviewOpt = findByTitle "Interstellar"
+match reviewOpt with
+| Some review -> printfn "%s" review.Summary
+| None -> printfn "No review available"
+
+
+[<EntryPoint>]
+let main argv = 
+    printfn "Press any key to exit program"
+    let user = Console.ReadKey() 
+    0 // return an integer exit code
+
